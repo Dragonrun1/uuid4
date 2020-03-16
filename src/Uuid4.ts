c49a3f90a8cd5fe26cc0a5280f98b164de032297
Michael Cummings <dragonrun1@gmail.com>
2020-03-15 19:02:17 -0700

    Added license info the Uuid4.ts file.

--- src/Uuid4.ts
+++ src/Uuid4.ts
@@ -1,6 +1,45 @@
-export class Uuid64Type {
-}
-
+/**
+ * Copyright (c) 2020 Michael Cummings. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the BSD-3 Clause License along with
+ * this program. If not, see
+ * <https://spdx.org/licenses/BSD-3-Clause.html>.
+ *
+ * You should be able to find a copy of this license in the LICENSE file.
+ *
+ * @author    Michael Cummings <dragonrun1@gmail.com>
+ * @copyright 2020 Michael Cummings
+ * @license   BSD-3-Clause
+ */
+/**
+ * Main UUID v4 (random) class.
+ */
 export class Uuid4 {
     /**
      * Generate a custom base 64 encoded UUID v4 (random).
